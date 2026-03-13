3d2b5a65b2ffb59e889ca68a90c85ec58c05d1a3
yuying huang <c5052631@newcastle.ac.uk>
2026-03-13 16:40:24 +0000

    feat: add staff entity and repository, update database schema

--- src/main/java/com/example/demo/Csc8019Application.java
+++ src/main/java/com/example/demo/Csc8019Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Csc8019Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Csc8019Application.class, args);
+	public static void main(String[] args) { SpringApplication.run(Csc8019Application.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/demo/entity/Staff.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "staff")
+public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    private String role;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+}
