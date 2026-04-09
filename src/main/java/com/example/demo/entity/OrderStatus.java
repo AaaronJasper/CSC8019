d7ba1d57a0430c46819cc535d4c3be0dba76a358
Devang Nadiyana <devangnadiyana25@gmail.com>
2026-04-09 14:48:18 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/entity/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+//This Enum and types in it defines strict categories for placing
+// a status of an order
+public enum OrderStatus {
+    NEW,
+    ACCEPTED,
+    PREPARING,
+    READY,
+    COLLECTED,
+    CANCELLED
+}
