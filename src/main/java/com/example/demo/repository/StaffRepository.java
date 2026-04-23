1c7d73ffb2c84de55c0c2386dfd031a81e9fafc4
Devang Nadiyana <devangnadiyana25@gmail.com>
2026-04-23 11:28:58 +0100

    Replace findByUsername with findByEmail method

--- src/main/java/com/example/demo/repository/StaffRepository.java
+++ src/main/java/com/example/demo/repository/StaffRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface StaffRepository extends JpaRepository<Staff,Long> {
 
-    Optional<Staff> findByUsername(String username);
+    Optional<Staff> findByEmail(String email);
 }
